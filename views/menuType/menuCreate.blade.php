4a5856f660f45978862e82161e705cd93d27579a
bnu <bnufactory@gmail.com>
2016-04-14 14:40:46 +0900

    패널 디자인 적용

--- views/menuType/menuCreate.blade.php
+++ views/menuType/menuCreate.blade.php
@@ -1,28 +1,18 @@
-<div class="panel menu_detail">
-    <div class="panel-heading">
-        <div class="row">
-            <p class="text-title">External Page Setting</p>
-
-            <div class="right_btn">
-                <button class="ico_gray"><i class="xi-angle-down"></i><span class="blind">닫기</span></button>
+<div class="panel-group">
+    <div class="panel">
+        <div class="panel-heading">
+            <div class="pull-left">
+                <p class="text-title">External Page Setting</p>
             </div>
         </div>
-    </div>
-    <div class="panel-body">
-        <div class="row_con">
+        <div class="panel-body">
             <p class="text-title">File Location</p>
 
-            <div class="row">
-                <div class="col-sm-10">
-                    <div>
-                        <dl>
-                            <dd><input type="text" name="includePath" class="form-control" value=""
-                                       placeholder="포함시킬 PHP file의 경로를 지정해주세요({{ base_path() }}/ 이하의 경로)"/>
-                            </dd>
-                        </dl>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <dl>
+                <dd><input type="text" name="includePath" class="form-control" value=""
+                 placeholder="포함시킬 PHP file의 경로를 지정해주세요({{ base_path() }}/ 이하의 경로)"/>
+             </dd>
+         </dl>
+     </div>
+     </div>
 </div>
--- views/menuType/menuEdit.blade.php
+++ views/menuType/menuEdit.blade.php
@@ -1,28 +1,20 @@
-<div class="panel menu_detail">
-    <div class="panel-heading">
-        <div class="row">
-            <p class="text-title">External Page Setting</p>
-
-            <div class="right_btn">
-                <button class="ico_gray"><i class="xi-angle-down"></i><span class="blind">닫기</span></button>
+<div class="panel-group">
+    <div class="panel">
+        <div class="panel-heading">
+            <div class="pull-left">
+                <h3 class="panel-title">External Page Setting</h3>
             </div>
         </div>
-    </div>
-    <div class="panel-body">
-        <div class="row_con">
-            <p class="text-title">File Location</p>
+        <div class="panel-body">
+            <div class="form-group">
+                <label>File Location</label>
 
-            <div class="row">
-                <div class="col-sm-10">
-                    <div>
-                        <dl>
-                            <dd><input type="text" name="includePath" class="form-control"
-                                       value="{{Input::old('includePath', $config->get('includePath'))}}"
-                                       placeholder="포함시킬 PHP file의 경로를 지정해주세요({{ base_path() }}/ 이하의 경로)"/>
-                            </dd>
-                        </dl>
-                    </div>
-                </div>
+                <dl>
+                    <dd><input type="text" name="includePath" class="form-control"
+                        value="{{Input::old('includePath', $config->get('includePath'))}}"
+                        placeholder="포함시킬 PHP file의 경로를 지정해주세요({{ base_path() }}/ 이하의 경로)"/>
+                    </dd>
+                </dl>
             </div>
         </div>
     </div>
